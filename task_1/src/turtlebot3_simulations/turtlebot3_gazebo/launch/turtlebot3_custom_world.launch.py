6b6dcccedb4bbc00ddf3ee80f89107354f4f4210
YashSharma-code <yash.sh.aqua@gmail.com>
2025-07-04 10:36:20 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   task_1/src/maps/my_map1.pgm 	new file:   task_1/src/maps/my_map1.yaml 	modified:   task_1/src/turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_custom_world.launch.py  Changes not staged for commit: 	modified:   task_1/src/Dataset-of-Gazebo-Worlds-Models-and-Maps (untracked content)
     Untracked files:
    	.gitignore

--- task_1/src/turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_custom_world.launch.py
+++ task_1/src/turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_custom_world.launch.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3
-#
-# Copyright 2019 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Authors: Joep Tool
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -26,12 +8,13 @@ from launch.substitutions import LaunchConfiguration
 from launch.actions import ExecuteProcess
 
 def generate_launch_description():
+    os.environ['TURTLEBOT3_MODEL'] = 'waffle_pi'
     launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose = LaunchConfiguration('x_pose', default='-2.0')
-    y_pose = LaunchConfiguration('y_pose', default='-0.5')
+    x_pose = LaunchConfiguration('x_pose', default='2.0')
+    y_pose = LaunchConfiguration('y_pose', default='1.0')
 
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
